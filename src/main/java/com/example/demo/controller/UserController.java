08fef5ec3ea991cee393af941838c29bcc6e4272
Adilet <dzhorobekov228@gmail.com>
2025-01-19 15:18:43 +0600

    new commit E

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
-public class wUserController {
+public class eUserController {
 
     @Autowired
     private UserService userService;
